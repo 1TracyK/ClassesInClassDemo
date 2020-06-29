06e40b9702bdc7f70ce4478270c8d9437736e033
Cruz Sanchez <csanchez@truecoders.io>
2020-06-29 12:01:31 -0500

    Demo going over classes

---
+++ dotNetConsole/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNetConsole
+{
+    class Person
+    {
+        //Property called Name
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        //Default constructor
+        public Person()
+        {
+
+        }
+
+        ////Constructor with a string parameter
+        public Person(string name)
+        {
+            //This will set the property Name with the value passed
+            //in from the constructor
+            this.Name = name;
+        }
+        
+        public Person(string name, string address)
+        {
+            //This will set the property Name with the value passed
+            //in from the constructor
+            this.Name = name;
+            this.Address = address;
+        }
+    }
+}
---
+++ dotNetConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dotNetConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of the List<T> class
+            List<int> nums = new List<int>();
+
+            int num = 0;
+
+            //Creating an instance of the Person class
+            Person p = new Person("Cruz", "1234 Acorn Ln");            
+
+            //Object Initializer Syntax
+            Person p2 = new Person() { Name = "John", Address = "4321 Nroca Ln" };            
+
+            Console.WriteLine($"p1's Name: {p.Name} p1's Address: {p.Address}");
+            Console.WriteLine(p2.Name);
+        }
+    }
+}
